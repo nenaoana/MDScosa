93bc60d789ea3a8a7cd3354f9813642e85cb266b
nenaoana <nena.oana@yahoo.com>
2020-04-06 12:52:04 +0300

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(...) {
   packageStartupMessage("To cite package MDScosa in publications use: 
 
-  Ioana-Elena Oana. 2020. MDScosa:  Multidimensional Scaling (MDS) for COSA data. 
+  Ioana-Elena Oana. 2020. MDScosa:  Multidimensional Scaling (MDS) for COSA data, 
   R package version 1.0. https://github.com/nenaoana/MDScosa")
 }
